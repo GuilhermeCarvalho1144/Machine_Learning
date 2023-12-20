c4a3dabba30b2d882bc5357a6e2a12b2f8b0346b
Guilherme C Pereira <guilhermecarvalho1144@gmail.com>
2023-12-19 22:51:10 -0300

    add yolo nas code

---
+++ Machine_Learning_Python/object_detection/face_detection/yolo_test.py
@@ -0,0 +1,90 @@
+import os
+import random
+import torch
+import requests
+from PIL import Image
+
+
+from super_gradients.training import Trainer, dataloaders, models
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import DetectionMetrics_050
+
+from super_gradients.training.dataloaders.dataloaders import (
+    coco_detection_yolo_format_train, 
+    coco_detection_yolo_format_val
+)
+
+from super_gradients.training.models.detection_models.pp_yolo_e import (
+    PPYoloEPostPredictionCallback
+)
+
+class config:
+    #trainer params
+    CHECKPOINT_DIR = './checkpoints' #specify the path you want to save checkpoints to
+    EXPERIMENT_NAME = 'finding-battleships/RUN_20231217_011657_858638' #specify the experiment name
+    #dataset params
+    DATA_DIR = '../../../../../Database/Face_detection/datasets/' #parent directory to where data lives
+    TRAIN_IMAGES_DIR = 'train/images' #child dir of DATA_DIR where train images are
+    TRAIN_LABELS_DIR = 'train/labels' #child dir of DATA_DIR where train labels are
+    VAL_IMAGES_DIR = 'valid/images' #child dir of DATA_DIR where validation images are
+    VAL_LABELS_DIR = 'valid/labels' #child dir of DATA_DIR where validation labels are
+    # if you have a test set
+    TEST_IMAGES_DIR = 'test/images' #child dir of DATA_DIR where test images are
+    TEST_LABELS_DIR = 'test/labels' #child dir of DATA_DIR where test labels are
+
+    CLASSES = ['Face']
+ #what class names do you have
+
+    NUM_CLASSES = len(CLASSES)
+
+    #dataloader params - you can add whatever PyTorch dataloader params you have
+    #could be different across train, val, and test
+    DATALOADER_PARAMS={
+    'batch_size': 16,
+    'num_workers':2
+    }
+
+    # model params
+    MODEL_NAME = 'yolo_nas_l' # choose from yolo_nas_s, yolo_nas_m, yolo_nas_l
+    PRETRAINED_WEIGHTS = 'coco' #only one option here: coco_detection_yolo_format_val
+
+
+trainer = Trainer(experiment_name=config.EXPERIMENT_NAME, 
+                  ckpt_root_dir=config.CHECKPOINT_DIR)
+
+test_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': config.DATA_DIR,
+        'images_dir': config.TEST_IMAGES_DIR,
+        'labels_dir': config.TEST_LABELS_DIR,
+        'classes': config.CLASSES
+    },
+    dataloader_params=config.DATALOADER_PARAMS
+)
+
+best_model = models.get(config.MODEL_NAME,
+                        num_classes=config.NUM_CLASSES,
+                        checkpoint_path='./checkpoints/finding-battleships/RUN_20231217_011657_858638/average_model.pth')
+
+trainer.test(model=best_model,
+            test_loader=test_data,
+            test_metrics_list=DetectionMetrics_050(score_thres=0.1, 
+               top_k_predictions=300, 
+               num_cls=config.NUM_CLASSES, 
+               normalize_targets=True, 
+               post_prediction_callback=PPYoloEPostPredictionCallback(
+                  score_threshold=0.01, 
+                  nms_top_k=1000, 
+                  max_predictions=300,
+                  nms_threshold=0.7)
+               ))
+
+DATASET_TEST = '../../../../../Database/Face_detection/datasets/test/images/'
+
+tpaths=[]
+for dirname, _, filenames in os.walk(DATASET_TEST):
+    for filename in filenames:
+        tpaths+=[(os.path.join(dirname, filename))]
+tpaths2=random.sample(tpaths,10)
+best_model.predict(tpaths2, conf=0.5).show()
+
