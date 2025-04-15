5e1742dbb816ce6ddcc1b130cd5367e291c385d9
Guilherme C Pereira <guilhermecarvalho1144@gmail.com>
2025-04-14 21:53:58 -0300

    add VAE decoder class

--- Machine_Learning_Python/Stable_Difusion/src/decoder.py
+++ Machine_Learning_Python/Stable_Difusion/src/decoder.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 from attetion import SelfAttention
 
 
-class VAR_ResidualBlock(nn.Module):
+class VAE_ResidualBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.groupnorm_1 = nn.GroupNorm(32, in_channels)
@@ -59,4 +59,57 @@ class VAE_AttentionBlock(nn.Module):
         # (batch_size, channels, H*W) -> (batch_size, channels, H, W)
         x = x.view(n, c, h, w)
         x += residual
+        return x
+
+
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            # (batch_size, 4, H/8, W/8) -> (batch_size, 4, H/8, W/8)
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+            # (batch_size, 4, H/8, W/8) -> (batch_size, 512, H/8, W/8)
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+            # (batch_size, 512, H/8, W/8) -> (batch_size, 512, H/8, W/8)
+            VAE_ResidualBlock(512, 512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            # (batch_size, 512, H/8, W/8) -> (batch_size, 512, H/4, W/4)
+            nn.Upsample(scale_factor=2),
+            # (batch_size, 512, H/4, W/4) -> (batch_size, 512, H/4, W/4)
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            # (batch_size, 512, H/4, W/4) -> (batch_size, 512, H/2, W/2)
+            nn.Upsample(scale_factor=2),
+            # (batch_size, 512, H/2, W/2) -> (batch_size, 512, H/2, W/2)
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            # (batch_size, 512, H/2, W/2) -> (batch_size, 256, H/2, W/2)
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+            # (batch_size, 256, H/2, W/2) -> (batch_size, 256, H, W)
+            nn.Upsample(scale_factor=2),
+            # (batch_size, 256, H, W) -> (batch_size, 256, H, W)
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            # (batch_size, 256, H, W) -> (batch_size, 128, H, W)
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            nn.GroupNorm(32, 128),
+            nn.SiLU(),
+            # (batch_size, 128, H, W) -> (batch_size, 3, H, W)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (batch_size, 4, H/8, W/8)
+        # reverse the scaling
+        x /= 0.18215
+        for module in self:
+            x = module(x)
+        # (batch_size, 3, H, W)
         return x
