a0527b612d0b61c95b7b369a163c9c92082dc999
Guilherme C Pereira <guilhermecarvalho1144@gmail.com>
2025-04-21 22:59:35 -0300

    add CrossAttention

--- Machine_Learning_Python/Stable_Diffusion/src/attention.py
+++ Machine_Learning_Python/Stable_Diffusion/src/attention.py
@@ -11,14 +11,14 @@ class SelfAttention(nn.Module):
         super().__init__()
         self.in_proj = nn.Linear(d_embed, 3 * n_heads, bias=in_proj_bias)
         self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
-        self.n_heads = n_heads
-        self.d_head = d_embed // n_heads
+        self.__n_heads = n_heads
+        self.__d_head = d_embed // n_heads
 
     def forward(self, x: torch.Tensor, casual_mask=False) -> torch.Tensor:
         # x: (batch_size, seq_len, d_emb)
         input_shape = x.shape
         batch_size, seq_len, d_emb = input_shape
-        inter_shape = (batch_size, seq_len, self.n_heads, self.d_head)
+        inter_shape = (batch_size, seq_len, self.__n_heads, self.__d_head)
 
         # define querry, key and value
         # (batch_size, seq_len, d_emb) -> (batch_size, seq_len, d_emb *3) -> slipt in 3 tensors of (batch_size, seq_len, d_emb)
@@ -37,7 +37,7 @@ class SelfAttention(nn.Module):
             # Mask the upper triangle from weigths
             mask = torch.ones_like(weigths, dtype=torch.bool).triu(1)
             weigths.masked_fill_(mask, -torch.inf)
-        weigths /= math.sqrt(self.d_head)
+        weigths /= math.sqrt(self.__d_head)
         weigths = F.softmax(weigths, dim=-1)
 
         # (batch_size, n_heads, seq_len, seq_len) @ (batch_size, n_heads, seq_len, d_head) -> (batch_size, n_heads, seq_len, d_head)
@@ -49,4 +49,45 @@ class SelfAttention(nn.Module):
         output = output.reshape(input_shape)
         output = self.out_proj(output)
 
+        return output
+
+
+class CrossAttention(nn.Module):
+    def __init__(
+        self,
+        n_heads: int,
+        d_emb: int,
+        d_cross: int,
+        in_proj_bias=True,
+        out_proj_bias=True,
+    ) -> None:
+        super().__init__()
+        self.q_proj = nn.Linear(d_emb, d_emb, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_cross, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_cross, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_emb, d_emb, bias=out_proj_bias)
+        self.__n_heads = n_heads
+        self.__d_head = d_emb // n_heads
+
+    def forward(self, x: torch.Tensor, contex: torch.Tensor) -> torch.Tensor:
+        # x: latend (batch_size, seq_len_Q, dims_Q)
+        # context (batch_size, seq_len_KV, dims_KV)
+        input_shape = x.shape
+        batch_size, seq_len, d_emb = input_shape
+        inter_shape = (batch_size, seq_len, self.__n_heads, self.__d_head)
+        # In the corss attention the Q comes from te latend and the K V from the contex
+        q = self.q_proj(x)
+        k = self.k_proj(contex)
+        v = self.v_proj(contex)
+
+        q = q.view(inter_shape).transpose(1, 2)
+        k = k.view(inter_shape).transpose(1, 2)
+        v = v.view(inter_shape).transpose(1, 2)
+        weigths = q @ k.transpose(-1, -2)
+        weigths /= math.sqrt(self.__d_head)
+        weigths = F.softmax(weigths, dim=-1)
+        output = weigths @ v
+        output = output.transpose(1, 2)
+        output = output.reshape(input_shape)
+        output = self.out_proj(output)
         return output
